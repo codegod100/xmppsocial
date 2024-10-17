f33178602b5fb69bf616af1306cd43c3f8339975
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-10-17 16:35:18 -0700

    long road

--- src/lib.rs
+++ src/lib.rs
@@ -34,68 +34,6 @@ pub async fn send_request(client: &mut Client<StartTlsServerConnector>, jid: &st
     Ok(())
 }
 
-pub async fn wait_stream(
-    client: &mut Client<StartTlsServerConnector>,
-    tx: Sender<String>,
-) -> Result<()> {
-    let mut stream_ended = false;
-    while !stream_ended {
-        if let Some(event) = client.next().await {
-            match event {
-                Event::Online { .. } => {
-                    debug!("online");
-                }
-                Event::Stanza(stanza) => match stanza {
-                    Stanza::Iq(iq) => {
-                        debug!("iq: {:?}", iq);
-                        if let IqType::Result(Some(element)) = iq.payload {
-                            let event = PubSub::try_from(element)?;
-                            match event {
-                                PubSub::Items(items) => {
-                                    debug!("items: {:?}", items);
-                                    for item in items.items {
-                                        if let Some(payload) = &item.payload {
-                                            let payload_s = String::from(payload);
-                                            debug!("payload: {:?}", payload_s);
-                                            let parsed = parser::parse(payload_s.as_bytes())?;
-                                            for entry in parsed.entries {
-                                                let mut link_s = String::from("");
-                                                for link in entry.links {
-                                                    link_s.push_str(&link.href);
-                                                    link_s.push_str("  ");
-                                                }
-
-                                                let s = format!(
-                                                    "{} :: {} :: {}",
-                                                    entry.title.unwrap().content,
-                                                    entry
-                                                        .content
-                                                        .unwrap_or_default()
-                                                        .body
-                                                        .unwrap_or_default(),
-                                                    link_s
-                                                );
-                                                debug!("entry: {:?}", s);
-                                                tx.send(s)?;
-                                            }
-                                        }
-                                    }
-                                }
-                                _ => debug!("event: {:?}", event),
-                            }
-                        }
-                    }
-                    _ => debug!("stanza: {:?}", stanza),
-                },
-                _ => debug!("event: {:?}", event),
-            }
-        } else {
-            stream_ended = true;
-        }
-    }
-    Ok(())
-}
-
 fn send_item(items: Items, tx: &Sender<String>) -> Result<()> {
     for item in items.items {
         if let Some(payload) = &item.payload {
@@ -110,7 +48,7 @@ fn send_item(items: Items, tx: &Sender<String>) -> Result<()> {
                 }
 
                 let s = format!(
-                    "{} :: {} :: {}",
+                    "{} :: {} :: {}\n\n",
                     entry.title.unwrap().content,
                     entry.content.unwrap_or_default().body.unwrap_or_default(),
                     link_s
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-use std::{
-    cell::RefCell,
-    env,
-    rc::Rc,
-    str::FromStr,
-    sync::{Arc, Mutex, MutexGuard},
-    thread,
-    time::Duration,
-};
+use std::{cell::RefCell, env, rc::Rc, str::FromStr, sync::Arc, thread, time::Duration};
 
 use anyhow::Result;
 use axum::{routing::get, Router};
 use dotenv::dotenv;
 use flume::{Receiver, Sender};
-use tokio::{pin, time};
+use tokio::{pin, sync::Mutex, time};
 use tokio_stream::StreamExt;
 use tokio_xmpp::{
     connect::StartTlsServerConnector,
@@ -22,7 +14,7 @@ use tokio_xmpp::{
     Client,
 };
 use tracing::{debug, error};
-use xmppsocial::{match_event, send_request, wait_stream};
+use xmppsocial::match_event;
 
 enum Command {
     GrabNode(String),
@@ -30,100 +22,88 @@ enum Command {
 }
 
 async fn command_loop(
-    tx: Sender<Command>,
-    rx: Receiver<Command>,
-    tx_xmpp: Sender<String>,
+    http_response: Receiver<String>,
+    content_request: Sender<String>,
 ) -> Result<()> {
     let jid = env::var("JID").expect("JID is not set");
     let jid = BareJid::from_str(&jid.clone())?;
     let password = env::var("PASSWORD").expect("PASSWORD is not set");
     let mut client = Client::new(jid.clone(), password);
-    for command in rx.iter() {
-        match command {
-            Command::GrabNode(jid) => {
-                debug!("grabbing node");
-                let s = "<pubsub xmlns='http://jabber.org/protocol/pubsub'>
+    loop {
+        tokio::select! {
+        Ok(jid) = http_response.recv_async() => {
+            debug!("grabbing {jid} from http request");
+            let s = "<pubsub xmlns='http://jabber.org/protocol/pubsub'>
             <items node='urn:xmpp:microblog:0'/>
           </pubsub>";
-                let e = Element::from_str(s)?;
-                let iqtype = IqType::Get(e);
-                let iq = Iq {
-                    to: Some(BareJid::from_str(&jid)?.into()),
-                    from: None,
-                    id: "232323".to_string(),
-                    payload: iqtype,
-                };
-                // let stanza = Stanza::Iq(iq);
-                client.send_stanza(iq.into()).await?;
-            }
-            Command::StartServer => {
-                debug!("before loop");
-                let sleep = time::sleep(Duration::from_secs(5));
-                tokio::pin!(sleep);
-                loop {
-                    tokio::select! {
-                        _ = &mut sleep => {
-                            debug!("operation timeout");
-                            break;
-                        }
-                        res = client.next() => {
-                            debug!("res: {:?}", res);
-                        }
-                    }
-                }
-                debug!("broke loop");
-                tx.send(Command::StartServer)?;
-            }
+            let e = Element::from_str(s)?;
+            let iqtype = IqType::Get(e);
+            let iq = Iq {
+                to: Some(BareJid::from_str(&jid)?.into()),
+                from: None,
+                id: "232323".to_string(),
+                payload: iqtype,
+            };
+            // let stanza = Stanza::Iq(iq);
+            client.send_stanza(iq.into()).await?;
+        },
+
+        Some(event) = client.next() => {
+            debug!("event: {:?}", event);
+            match_event(event, &content_request)?;
+        }
+
+
         }
     }
-    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
-    // rustls::crypto::ring::default_provider()
-    //     .install_default()
-    //     .unwrap();
+
     dotenv().ok();
 
-    // channel for http request
-    let (tx, rx) = flume::unbounded();
-
-    // // channel for xmpp request
-    let (tx_xmpp, rx_xmpp) = flume::unbounded();
-
-    tx.send(Command::StartServer)?;
-    let mx = tx.clone();
-    // command loop, lets put everything inside of this
-    tokio::spawn(async move { command_loop(mx, rx, tx_xmpp).await });
-
-    let res = run_server(tx, rx_xmpp).await;
-    if let Err(e) = res {
-        error!("error starting server: {}", e);
-    }
+    let (http_request, http_response) = flume::unbounded();
+    let (content_request, content_response) = flume::unbounded();
+    tokio::spawn(run_server(http_request, content_response));
+    command_loop(http_response, content_request).await?;
 
     Ok(())
 }
 
-async fn run_server(tx: Sender<Command>, rx_xmpp: Receiver<String>) -> Result<()> {
-    let app = Router::new().route("/", get(|| handler(tx, rx_xmpp)));
+async fn run_server(
+    http_request: Sender<String>,
+    content_response: Receiver<String>,
+) -> Result<()> {
+    let app = Router::new().route("/", get(|| handler(http_request, content_response)));
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
     debug!("listening on http://{}", listener.local_addr()?);
     axum::serve(listener, app).await?;
     Ok(())
 }
 
-async fn handler(tx: Sender<Command>, rx_xmpp: Receiver<String>) -> String {
+async fn handler(http_request: Sender<String>, content_response: Receiver<String>) -> String {
     debug!("inside handler");
     // todo change this to a request param
     let jid = env::var("JID").expect("JID is not set");
     let jid = BareJid::from_str(&jid.clone()).unwrap();
 
-    let res = tx.send(Command::GrabNode(jid.to_string()));
-    debug!("grabbed node");
+    let res = http_request.send_async(jid.to_string()).await;
     if let Err(e) = res {
         error!("error sending request: {}", e);
     }
-    rx_xmpp.recv().unwrap()
+    // let sleep = time::sleep(Duration::from_secs(5));
+    // tokio::pin!(sleep);
+    let mut s = String::new();
+    // loop {
+    //     tokio::select! {
+    //         item = cm.rx_node.recv_async() => {
+    //             debug!("web output: {:?}", item.clone().unwrap());
+    //             s.push_str(&item.unwrap());
+    //         }
+    //     }
+    // }
+
+    s
 }
